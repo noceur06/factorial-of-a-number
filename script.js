cae95c98c796596a613f1115db0bfdc84f9b5982
noceur06 <ya0558494@gnail.com>
2026-01-07 18:37:30 +0530

    Initial commit - factorial program

---
+++ script.js
@@ -0,0 +1,43 @@
+function fectorial(num){
+    let result = 1;
+    for(let i = num ; i >= 1; i--){
+         result *= i;
+    }
+    return result;
+}
+
+console.log(fectorial(18));
+
+
+//with while loop
+
+
+// function fectiorial(num){
+//     let result = 1;
+//     let i = num;
+//     while(i >= 1){
+//         result *= i;
+//         i--;
+//     }
+
+//     console.log(result)
+// }
+
+// fectiorial(5)
+
+
+
+
+//with prompt
+
+// function fectorial(num){
+//     let result = 1;
+//     for(let i = num ; i >= 1; i--){
+//          result *= i;
+//     }
+//     return result;
+// }
+
+// let forPrompt = Number(prompt("enter a number for fectorial"))
+
+// console.log(fectorial(forPrompt));
